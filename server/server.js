ae88449847ca60db89eaf4914c68eff0e81a25f2
Disha Handa <disha55handa@gmail.com>
2025-01-22 00:40:47 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: ["https://nexmall.vercel.app"],
+        origin: "https://nexmall.vercel.app",
         methods: ["POST", "GET"],
         credentials: true
     }
