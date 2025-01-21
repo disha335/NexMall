e6eb67696d20024f02c79628acbc320f0a3bdb9e
Disha Handa <disha55handa@gmail.com>
2025-01-22 00:23:46 +0530

    Update server.js for frontend deployment

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: [],
+        origin: ["https://nexmall.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
