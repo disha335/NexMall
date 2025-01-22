18bfd710fee8141658f3deb77a778a4b56f037ac
Disha Handa <disha.handa@geminisolutions.com>
2025-01-22 23:15:55 +0530

    updating api.js

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: "https://nex-mall.vercel.app",
-  // baseURL: "http://localhost:5000/",
   withCredentials: true, // Include cookies if needed
 });
 
